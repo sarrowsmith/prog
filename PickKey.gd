ae9ba1e3031f0d91e996a34df6d7e230b54a1d77
Sion Arrowsmith <sion.arrowsmith@gmail.com>
2021-05-01 17:24:34 +0100

    Fix stuff

--- PickKey.gd
+++ PickKey.gd
@@ -2,8 +2,9 @@ extends OptionButton
 
 
 func _unhandled_input(event):
-	if KEY_A <= event.scancode and event.scancode <= KEY_G:
-		var id = event.scancode - KEY_A
-		if event.shift:
-			id += 8
-		selected = get_item_index(id)
+	if event is InputEventKey:
+		if KEY_A <= event.scancode and event.scancode <= KEY_G:
+			var id = event.scancode - KEY_A
+			if event.shift:
+				id += 8
+			selected = get_item_index(id)
--- PickMode.gd
+++ PickMode.gd
@@ -2,8 +2,9 @@ extends OptionButton
 
 
 func _unhandled_input(event):
-	if KEY_1 <= event.scancode and event.scancode <= KEY_7:
-		selected = event.scancode - KEY_1
+	if event is InputEventKey:
+		if KEY_1 <= event.scancode and event.scancode <= KEY_7:
+			selected = event.scancode - KEY_1
 
 func set_mode(idx: int):
 	clear()
